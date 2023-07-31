99adad5102523a21c77c532b62e91bc28628b2d0
Crufro <45433487+Crufro@users.noreply.github.com>
2023-07-30 22:25:07 -0400

    Update MiiladyViewer.js
    Lighting

--- components/MiiladyViewer.js
+++ components/MiiladyViewer.js
@@ -1,4 +1,3 @@
-// MiiladyViewer
 import { Suspense } from 'react';
 import { Canvas } from '@react-three/fiber';
 import MiiladyModel from '../components/utils3D/MiiladyModel';
@@ -12,7 +11,9 @@ const MiiladyViewer = ({
 }) => (
   <div style={{ position: 'relative', zIndex: 2, width: '100vw', height: '75vh' }}>
     <Canvas style={{ width: '100%', height: '100%' }}>
-      <ambientLight intensity={0.75} />
+      <ambientLight intensity={1} color={'#8498ad'} />
+      <directionalLight position={[5, 10, 20]} intensity={1} color={'#ffe4b5'} />
+      <pointLight position={[20, 0, 20]} intensity={0.5} color={'#ffc3b5'} />
       <Controls fov={fov} cameraPosition={cameraPosition} targetPosition={targetPosition} />
       <Suspense fallback={null}>
         {models.map((model, index) => (
