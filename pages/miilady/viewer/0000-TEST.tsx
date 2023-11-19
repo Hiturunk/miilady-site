faf5f84740b6a7cf9285b755ca262f03b297c999
Crufro <45433487+Crufro@users.noreply.github.com>
2023-11-19 13:04:20 -0500

    fix compile issue

--- pages/miilady/viewer/0000-TEST.tsx
+++ pages/miilady/viewer/0000-TEST.tsx
@@ -1,21 +1,34 @@
 import React, { Suspense, useRef, useEffect } from 'react';
-import { Canvas } from '@react-three/fiber';
+import { Canvas, useFrame } from '@react-three/fiber';
 import { OrbitControls, useGLTF } from '@react-three/drei';
 import styles from '/styles/ModelViewer.module.css'; // adjust the path as necessary
+import * as THREE from 'three';
 
 function Model() {
-  const model = useGLTF('/NFT/0000-TEST/0000.gltf');
-  const modelRef = useRef();
+  const model = useGLTF('/NFT/0000-TEST/0000-ANIMATED.gltf');
+  const modelRef = useRef<THREE.Object3D>(null);
+  const mixer = useRef();
 
   useEffect(() => {
     if (modelRef.current) {
+      // Set initial rotation
       const xRotation = 0; // e.g., Math.PI / 2 for 90 degrees
       const yRotation = 1.6; // e.g., Math.PI for 180 degrees
       const zRotation = 0; // e.g., Math.PI / 4 for 45 degrees
-  
       modelRef.current.rotation.set(xRotation, yRotation, zRotation);
+
+      // Animation setup
+      mixer.current = new THREE.AnimationMixer(modelRef.current);
+      if (model.animations.length > 0) {
+        const action = mixer.current.clipAction(model.animations[0]);
+        action.play();
+      }
     }
   }, [model]);
+
+  useFrame((state, delta) => {
+    mixer.current?.update(delta);
+  });
 
   return <primitive object={model.scene} ref={modelRef} />;
 }
@@ -23,7 +36,7 @@ function Model() {
 export default function Viewer() {
   const cameraRef = useRef();
   const controlsRef = useRef();
-  
+
   // Adjust these values as needed for your model
   const cameraPosition = [0, 1, 2]; // This sets the distance of the camera from the model
   const fov = 50; // Field of view
@@ -48,7 +61,7 @@ export default function Viewer() {
           enablePan={false}
           maxPolarAngle={Math.PI / 1.5} // Limit the vertical rotation to the upper hemisphere
           minPolarAngle={1}            // Prevent the camera from going below the horizon
-          target={[0, 0.8, 0]} 
+          target={[0, 1, 0]} 
         />
       </Canvas>
     </div>
